447baf84c3e04896260d4220a6517a5ec15ddd16
marwai <mtse@spartaglobal.com>
2020-07-14 10:49:32 +0100

    destination

---
+++ Destinations/Destinations.py
@@ -0,0 +1,5 @@
+class Destinations():
+    def flight_path(self, destination, origin, duration ):
+        self.destination = destination
+        self.origin = origin
+        self.duration = duration
