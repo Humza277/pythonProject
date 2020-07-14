f899f8ac5b24b739661c144507ea5f8914229c6e
marwai <mtse@spartaglobal.com>
2020-07-14 12:41:02 +0100

    db

---
+++ Database/airport_queries.py
@@ -0,0 +1,19 @@
+from database_connections import *
+import panda as pd
+
+# database_OOP parent class
+class Airport_queries:
+    # static method in python, static includes the whole method. self is part of the instance,
+    # methods are at a class level once static methods assigned
+    # no static variables but are class variables, doesn't remember it's state level
+    def All_products(self):
+        object = Database_OOP()  # object of Database_OOP class
+        # cursor runs all queries
+        cursor = object.establishing_connection()  # cursor required to run queries in databse
+        query_result = f"insert into user_name(id, name) values ('pyodbc', 'awesome library')"
+        rows = cursor.execute(query_result)
+        for row in rows:
+            print(row)
+
+        cursor.execute()
+        cnxn.commit()
---
+++ Database/database_connections.py
@@ -0,0 +1,21 @@
+import pyodbc
+# Creating a class with singular method
+class Database_OOP:
+    # method connects to db
+    server = 'databases2.spartaglobal.academy'  #
+    database = 'dangus'
+    username = 'SA'
+    password = 'Passw0rd2018'
+
+    # when this method, cursor is returned
+    def establishing_connection(self):
+        connections = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password
+        #
+        try:
+            with pyodbc.connect(connections, timeout=5) as connection:
+                print("Connection did not time out")
+        except:
+            print("Connection Time out")
+        else:
+            cursor = connection.cursor() # variable set to cursor
+            return cursor # return the value of allocated position of the cursor
