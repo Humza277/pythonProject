b37df7430f4f35d6b0369bad03aafa9fa68063cf
hmalak <hmalak@spartaglobal.com>
2020-07-15 14:44:20 +0100

    file being truncated works now

--- Database/airport_queries.py
+++ Database/airport_queries.py
@@ -1,34 +1,49 @@
+import numpy as np
+import pandas as pd
 from database_connections import *
 from BookingApp.bookingMain import *
-import panda as pd
+
 
 # database_OOP parent class
 class Airport_queries:
     def __init__(self):
         pass
 
-    # def passenger_info(self):
-    #     object = Bookingapp.createUser()  # object of Database_OOP class
-    #     # cursor runs all queries
-    #     cursor = object.establishing_connection()  # cursor required to run queries in databse
-    #     # code
-    #     query_result = f"insert into user_name(First_Name,Last_Name,Passport_Number,Date_of_Birth) values " \
-    #                    f"({fname}, {lname},{passportNumber},{Dob})"
-    #     rows = cursor.execute(query_result)
-    #     for row in rows:
-    #         print(row)
+    def passenger_info(self):
+        object = Bookingapp.userStore()  # object of Database_OOP class
+        # cursor runs all queries
+        cursor = object.establishing_connection()  # cursor required to run queries in databse
+        # code
+        query_result = f"insert into user_name(First_Name,Last_Name,Passport_Number,Date_of_Birth) values " \
+                       f"({fname}, {lname},{passportNumber},{Dob})"
+        rows = cursor.execute(query_result)
+        for row in rows:
+            print(row)
 
     def send_to_database(self):
-        filename = "passengerlist.csv"
+        df = pd.read_csv('passengerlist.csv')
+        first_column = df.columns[0]
+        df = df.drop([first_column], axis=1)
+        df.to_csv('passengerlist.csv', index=False)
+
+        d = Database()
+        cursor = d.create_cursor()
+
+        cursor.execute()
+
+
+
+
+
+        #filename = "passengerlist.csv"
         # opening the file with w+ mode truncates the file
-        f = open(filename, "w+") # opening the file with w+ mode truncates the file
-        f.close()
+        #f = open(filename, "a+") # opening the file with w+ mode truncates the file
+        #f.close()
         # data = pd.read_csv('passengerlist.csv')
         # df = pd.DataFrame(data, columns=['Passenger_ID', 'First_Name',
         #                                  'Last_Name', ' Date_Of_Birth', 'Passport_Number'])
-        # d = Database()
-        # cursor = d.create_cursor()
-        # cursor.execute()
+        #
         # pass
+
 u = Airport_queries()
 u.send_to_database()
