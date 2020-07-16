702c1467a242286a81d8834dea8f0738e9deebe5
hmalak <hmalak@spartaglobal.com>
2020-07-16 21:34:23 +0100

    added hashing to the login method of the airport assistant

--- Database/test.py
+++ Database/test.py
@@ -1,24 +1,29 @@
-from database_connections import Database
-
-class Airport_Assistant_Staff:
-
-    @staticmethod
-    def flight_attendant_list():
-        object = Database()
-        # cursor runs all queries
-        cursor = object.establishing_connection()  # cursor required to run queries in database
-        query_result = "SELECT City FROM Destination"
-        rows = cursor.execute(query_result)
-        # passenger_choice = input("Please type in your choice: \n")
-        for row in rows:
-            print(row)
-        # if passenger_choice.capitalize() in rows:
+# from database_connections import Database
+import pandas as pd
+# class Airport_Assistant_Staff:
+#
+#     @staticmethod
+#     def flight_attendant_list():
+#         object = Database()
+#         # cursor runs all queries
+#         cursor = object.establishing_connection()  # cursor required to run queries in database
+#         query_result = "SELECT City FROM Destination"
+#         rows = cursor.execute(query_result)
+#         df = pd.DataFrame(rows, columns=['City'])
+#         print(df)
+        #passenger_choice = input("Please type in your choice: \n")
+        # for row in rows:
+        #     print(row)
+        # df1 = df[df['City'].str.contains('oslo')]
+        #print(df1)
+        # if
         #     print("success")
         # else:
         #     print("oh hell no")
         # df = pd.DataFrame(rows, columns=['City'])
         # print(df)
 
-a = Airport_Assistant_Staff()
-a.flight_attendant_list()
+
+# a = Airport_Assistant_Staff()
+# a.flight_attendant_list()
 
