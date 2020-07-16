0617c0a8a6f589d4d51b96827980c73a77600287
Anais <a6anaistang@hotmail.co.uk>
2020-07-16 19:12:31 +0100

    added dummy cities csv data

--- Destinations/citiestoDatabase.py
+++ Destinations/citiestoDatabase.py
@@ -3,17 +3,6 @@ import pandas as pd
 import os
 from Destinations.databaseconnect import Databases
 
-"""
-CSV files to import 
-List of destinations (dangus airline)
-List of short haul flights (SELECT cities where flight type = short haul)
-List of long haul flights (SELECT cities where flight type = long haul)
-List of cheapest flights (import from destinations.py, import flight budget method)
-Databases to display = list of cities
-Passenger details (passengerlist.csv)
-
-"""
-# d = Databases()
 
 class DummyCities(Databases):
 
@@ -23,6 +12,8 @@ class DummyCities(Databases):
         df = pd.DataFrame(data, columns=['country', 'city', 'country_code'])
         print(df)
 
+    def checking_city_exists():
+        pass
 #
 # dc = DummyCities
 # dc.csv_to_dataframe()
